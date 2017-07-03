d1ef728f3ce86091928bb10988a758f46f716dca
Ananta Yudica <nta040689@gmail.com>
2017-07-04 02:26:07 +0700

    Add Object module in module.exports

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module : {
+      loaders: [
+          {
+              test: /.jsx?$/,
+              loader: 'babel-loader',
+              exclude: /node_module/,
+              query: {
+                  presets: ['es2015', 'react']
+              }
+          }
+      ]
   }
 };
